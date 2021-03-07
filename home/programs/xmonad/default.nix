f96feecfa7962f6db9360db07d214cdb72d0504d
kotval <11917243+kotval@users.noreply.github.com>
2021-03-07 17:48:00 -0600

    Update default.nix
    update xmonad for thinkpad

--- home/programs/xmonad/default.nix
+++ home/programs/xmonad/default.nix
@@ -5,7 +5,6 @@ let
     ${pkgs.util-linux}/bin/setterm -blank 0 -powersave off -powerdown 0
     ${pkgs.xorg.xset}/bin/xset s off
     ${pkgs.xcape}/bin/xcape -e "Hyper_L=Tab;Hyper_R=backslash"
-    ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-0 --mode 3840x2160 --rate 30.00
   '';
 
   polybarOpts = ''
@@ -17,7 +16,7 @@ let
 in
 {
   xresources.properties = {
-    "Xft.dpi" = 180;
+    "Xft.dpi" = 125.37;
     "Xft.autohint" = 0;
     "Xft.hintstyle" = "hintfull";
     "Xft.hinting" = 1;
